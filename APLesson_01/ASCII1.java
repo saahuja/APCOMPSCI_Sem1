eb73b7a2547ecda4eced8f683d86993cd91ea51c
Sahil Ahuja <sahilahuja99@live.com>
2016-09-06 08:11:39 -0700

    Added Files

---
+++ APLesson_01/ASCII1.java
@@ -0,0 +1,22 @@
+public class ASCII1
+{
+	public static void main(String[]args)
+	{
+		System.out.println("        ~+	");
+		System.out.println("    ");
+		System.out.println("                *       +  ");
+		System.out.println("          \'                  |   ");
+		System.out.println("      ()    .-.,=\"``\"=.    - o -   ");
+		System.out.println("            \'=/_       \\     |   ");
+		System.out.println("         *   |  \'=._    \\     `=./`,        \'   ");
+		System.out.println("              \\     `=./`,        '  ");
+		System.out.println("            .   \'=.__.=\' `=\'      *   ");
+		System.out.println("   +                         +   ");
+		System.out.println("        O      *        '       .   ");
+		
+		
+		
+		System.out.println("                   Saturn           ");
+
+	}
+}
---
+++ APLesson_01/ASCII2.java
@@ -0,0 +1,35 @@
+public class ASCII2
+{
+	public static void main(String[]args)
+	{
+		System.out.println("         _________________________.                                 ");
+		System.out.println("        / _____________________  /|                                  ");
+		System.out.println("       / / ___________________/ / |                                  ");
+		System.out.println("      / / /| |               / /  |                                  ");
+		System.out.println("     / / / | |              / / . |                                  ");
+		System.out.println("    / / /| | |             / / /| |                                 ");
+		System.out.println("   / / / | | |            / / / | |                                  ");
+		System.out.println("  / / /  | | |           / / /| | |                                  ");
+		System.out.println(" / /_/___| | |__________/ / / | | |                                  ");
+		System.out.println("/________| | |___________/ /  | | |                                  ");
+		System.out.println("| _______| | |__________ | |  | | |                                  ");
+		System.out.println("| | |    | | |_________| | |__| | |                                  ");
+		System.out.println("| | |    | |___________| | |____| |                                  ");
+		System.out.println("| | |   / / ___________| | |_  / /                                  ");
+		System.out.println("| | |  / / /           | | |/ / /                                  ");
+		System.out.println("| | | / / /            | | | / /                                  ");
+		System.out.println("| | |/ / /             | | |/ /                                  ");
+		System.out.println("| | | / /              | | ' /        ");
+		System.out.println("| | |/_/_______________| |  /        ");
+		System.out.println("| |____________________| | /        ");
+		System.out.println("|________________________|/        ");
+		
+	}
+		
+		
+		
+		
+		
+}
+	
+	
