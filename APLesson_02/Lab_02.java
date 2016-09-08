189520a09dc56859428a309874c9131c6d763890
Sahil Ahuja <sahilahuja99@live.com>
2016-09-08 07:56:08 -0700

    Update Lab_02

--- APLesson_02/Lab_02.java
+++ APLesson_02/Lab_02.java
@@ -7,9 +7,8 @@ public class Lab_02
 		int b=12;
 		int product=a*b;
 		System.out.println("The product of 25 and 12 is"+" "+product);
-		
-		
-		
+		System.out.println("                   ");
+	
 		System.out.println("Concatenation");
 		String name="Sahil Ahuja";
 		String address="13065 Caminito Bautizo";
@@ -19,7 +18,7 @@ public class Lab_02
 		System.out.println(name);
 		System.out.println(address);
 		System.out.println(city+","+ " "+state+" "+zip);
-	
+		System.out.println("                   ");
 		
 		
 		System.out.println("Complex Calculation");
@@ -28,13 +27,6 @@ public class Lab_02
 		int h=22;
 		int SurfaceArea= 2*((l*w)+(w*h)+(h*l));
 		System.out.println("The surface area of a cuboid with length=5, width=5, and height=22 is" + " "+SurfaceArea);
-
-		
-	
-	
-	
-	
-	
 	}
 	
 	
