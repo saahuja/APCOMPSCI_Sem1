0f0c77e19033554e19125bc493ba4f2348589472
saahuja <sahilahuja99@live.com>
2016-10-28 09:19:37 -0700

    added labs

---
+++ APLesson_05/APLab_05_2/APLab_05_3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class APLab_05_3
+{
+		public static void main(String[]args)
+		Scanner kb= new Scanner(System.in);
+		System.out.println("*Enter Darth Vader*");
+		System.out.println("what planet will you choose to invade, Lord Vader:");
+		System.out.println("Tatoinne");
+		System.out.println("Hoth");
+		System.out.println("Endor");
+		String input= kb.next();
+		
+		if(kb.equals)
+}
--- APLesson_05/APLab_05_2/APLab_05_4.java
+++ APLesson_05/APLab_05_2/APLab_05_4.java
@@ -1,23 +1,49 @@
 import java.util.Scanner;
 public class Lab_05_4
 {
-	static String Usernames = "";
-	static String Passwords = "";
+	static String usernames = "";
+	static String passwords = "";
 	public static void main(String[]args)
 	{
-		System.out.println("[Password Gateway]");
-		CheckPass();
+		System.out.println("Password Program");
+		passcheck();
 	}
 	
-	public static void CheckPass()
+	public static void passcheck()
 	{
-		Scanner Input = new Scanner(System.in);
-		System.out.println("Enter your USERNAME:");
-		Usernames = Input.next();
-		System.out.println("Enter your PASSWORD:");
-		Passwords = Input.next();
-		PassCheck(Usernames,Passwords);
-	}
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Enter your username:");
+		usernames = kb.next();
+		System.out.println("Enter your password:");
+		passwords = kb.next();
+		security(use,pas);
 	
 
 	}
+	public static void security(String use, String pas)
+	{
+		if(use.Equals("sahil")&&pas.Equals("sahil"))
+		{
+			System.out.println("Access Granted");
+		}
+		else if(use.Equals("sahil")&&!pas.Equals("sahil"))
+		{
+			System.out.println("Your password is incorrect");
+			passcheck();
+		}
+		else if(!use.Equals("sahil")&&pas.Equals("sahil"))
+		{	
+			System.out.println("Your username is incorrect");	
+			passcheck();
+		}
+		else if(!use.Equals("sahil")&&!pas.Equals("sahil"))
+		{
+			System.out.println("Your username and password is incorrect");
+			passcheck();
+		}
+	
+	}
+	
+	
+	
+}	
