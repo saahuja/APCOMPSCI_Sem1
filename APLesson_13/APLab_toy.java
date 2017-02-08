845d7b1e49d34738a1298cf2dffe43f4d9a8e393
saahuja <sahilahuja99@live.com>
2017-02-08 10:25:08 -0800

    added labs

---
+++ APLesson_13/APLab_toy.java
@@ -0,0 +1,49 @@
+import java.util.Scanner
+public class APLab_toy
+{
+	private String name;
+	private int count;
+
+	public APLab_toy()
+	{
+		this.name=" ";
+		this.count=0                                                                                                                                       ;
+	}
+	
+	public APLab_toy(String n, double c)
+	{
+		this.name= n;
+		this.count= c;
+	}
+	
+	public String toString()
+	{
+		
+		
+		
+	}
+	
+	public default to String()
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
